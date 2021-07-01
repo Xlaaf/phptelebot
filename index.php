48652fcfdd5e99ea1732adf0ebb3dc97ae88b135
exolop <80510724+Xlaaf@users.noreply.github.com>
2021-07-02 00:00:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+$telegram = new Telegram('YOUR TELEGRAM TOKEN HERE');
+
+$req = $telegram->getUpdates();
+
+for ($i = 0; $i < $telegram-> UpdateCount(); $i++) {
+	// You NEED to call serveUpdate before accessing the values of message in Telegram Class
+	$telegram->serveUpdate($i);
+	$text = $telegram->Text();
+	$chat_id = $telegram->ChatID();
+
+	if ($text == '/start') {
+		$reply = 'Working';
+		$content = array('chat_id' => $chat_id, 'text' => $reply);
+		$telegram->sendMessage($content);
+	}
+	// DO OTHER STUFF
+}
